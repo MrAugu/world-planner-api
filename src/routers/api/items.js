031bda750566a11b3531ffd52c62b4429c36d3e8
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-08-15 18:51:06 +0300

    feat(collisions): For spread types 1, 4 and 3

--- src/routers/api/items.js
+++ src/routers/api/items.js
@@ -1,3 +1,4 @@
+const { isSymbol } = require("lodash");
 const { paginate, parseQueryInteger, codes } = require("../../../lib/index");
 
 const routes = async (fastify) => {
@@ -40,7 +41,7 @@ module.exports = routes;
 //  Up   Right  Down  Left
 
 function getCollisions (item) {
-  const collisions = {};
+  let collisions = {};
   if ([1, 4].includes(item.collision_type) && item.spread_type === 2) {
     collisions[[true, true, true, true]] = { texture_x: item.texture_x, texture_y: item.texture_y };
     collisions[[false, true, true, true]] = { texture_x: item.texture_x + 1, texture_y: item.texture_y };
@@ -75,5 +76,18 @@ function getCollisions (item) {
     collisions[[false, true, false, false]] = { texture_x: item.texture_x + 6, texture_y: item.texture_y + 1 };
     collisions[[false, false, false, true]] = { texture_x: item.texture_x + 5, texture_y: item.texture_y + 1 };
     collisions[[false, true, false, true]] = { texture_x: item.texture_x + 7, texture_y: item.texture_y + 1 };
+  } else if ([1, 4].includes(item.collision_type) && item.spread_type === 1) {
+    collisions = {};
+  } else if ([1, 4].includes(item.collision_type) && item.spread_type === 4) {
+    collisions[[true, false, false, false]] = { texture_x: item.texture + 1, texture_y: item.texture_y };
+    collisions[[false, true, false, false]] = { texture_x: item.texture + 2, texture_y: item.texture_y };
+    collisions[[false, false, true, false]] = { texture_x: item.texture + 3, texture_y: item.texture_y };
+    collisions[[false, false, false, true]] = { texture_x: item.texture, texture_y: item.texture_y };
+    collisions[[false, false, false, false]] = { texture_x: item.texture + 4, texture_y: item.texture_y };
+  } else if ([1, 4].includes(item.collision_type) && item.spread_type === 3) {
+    collisions[[false, true]] = { texture_x: item.texture_x, texture_y: item.texture_y };
+    collisions[[true, true]] = { texture_x: item.texture_x + 1, texture_y: item.texture_y };
+    collisions[[true, false]] = { texture_x: item.texture_x + 2, texture_y: item.texture_y };
+    collisions[[false, false]] = { texture_x: item.texture_x + 3, texture_y: item.texture_y };
   }
 }
