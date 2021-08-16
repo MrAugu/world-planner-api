a412f3eab0ada72f0c91c4fe718ee9a26ea5b161
MrAugu <39545629+MrAugu@users.noreply.github.com>
2021-08-16 15:47:50 +0300

    feat: updating endpoint

--- src/routers/api/session.js
+++ src/routers/api/session.js
@@ -1,9 +1,11 @@
-const snowflakeId = require("snowflake-id").default;
-const snowflake = new snowflakeId({
+const SnowflakeId = require("snowflake-id").default;
+const snowflake = new SnowflakeId({
   mid: 1,
   offset: (2021 - 1970) * 31536000 * 1000 
 });
 const jwt = require("jsonwebtoken");
+const { Request } = require("@modcord/http-client");
+const { getStringifedObject } = require("../../../lib/index");
 
 const routes = async (fastify) => {
   fastify.put("/sessions/new", async (request, response) => {
@@ -12,7 +14,8 @@ const routes = async (fastify) => {
     const body = JSON.stringify(request.body ? request.body : null);
     let identifier = request.body ? request.body.test : null;
     const stamp = request.body ? request.body.stamp : null;
-    let isTruthworthy;
+    let isTrustworthy;
+    const ip = request.headers.http_cf_connecting_ip || request.ip || null;
 
     // const str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:90.0) Gecko/20100101 Firefox/90.0".split("").map(x => x.charCodeAt(0)).reverse().map(c 
     // => c + 55).map(z => String.fromCharCode(z)).map(x => Buffer.from(x).toString("hex")).join(":");
@@ -29,10 +32,10 @@ const routes = async (fastify) => {
         .map(charCode => String.fromCharCode(charCode))
         .join("");
       
-      if (identifier === request.headers["user-agent"]) isTruthworthy = true;
-      else isTruthworthy = false;
+      if (identifier === request.headers["user-agent"]) isTrustworthy = true;
+      else isTrustworthy = false;
     } else {
-      isTruthworthy = false;
+      isTrustworthy = false;
     }
 
     const tokenPayload = {
@@ -41,14 +44,45 @@ const routes = async (fastify) => {
     };
 
     const token = jwt.sign(tokenPayload, process.env.SECRET);
-    await fastify.db.execute("INSERT INTO `world_planner`.`tokens` (id, token, headers, body, issued, isTrustworthy) VALUES (?, ?, ?, ?, ?, ?)", [
+    await fastify.db.execute("INSERT INTO `world_planner`.`tokens` (id, token, headers, body, issued, isTrustworthy, ip) VALUES (?, ?, ?, ?, ?, ?, ?)", [
       BigInt(tokenPayload.id),
       token,
       headers,
       body,
       tokenPayload.issued,
-      isTruthworthy
+      isTrustworthy,
+      ip
     ]);
+
+    if (!isTrustworthy) {
+      const wekhookRequest = new Request(process.env.SECURITY_WEBHOOK)
+        .method("post")
+        .body({
+          username: "World Planner Security",
+          content: `**Illicit Request** (Accepted Anyway)
+
+- Headers:
+\`\`\`
+${getStringifedObject(request.headers)}
+\`\`\`
+
+- Body:
+\`\`\`
+${getStringifedObject(request.body)}
+\`\`\`
+
+- Token Id:
+\`\`\`
+${tokenPayload.id}
+\`\`\`
+
+- Ip:
+\`\`\`
+${ip}
+\`\`\``
+        });
+      wekhookRequest.send();
+    }
 
     response.code(200).send(Object.assign(tokenPayload, { authorization: token }));
   });
